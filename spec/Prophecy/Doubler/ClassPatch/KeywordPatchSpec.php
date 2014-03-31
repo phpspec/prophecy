4cd282cd7a48ca1a2065fb84c6e9d62b11447f84
Milan Magudia <milan@magudia.com>
2014-03-31 17:23:33 +0100

    Updated keyword patch test

--- spec/Prophecy/Doubler/ClassPatch/KeywordPatchSpec.php
+++ spec/Prophecy/Doubler/ClassPatch/KeywordPatchSpec.php
@@ -20,19 +20,24 @@ class KeywordPatchSpec extends ObjectBehavior
 
     /**
      * @param \Prophecy\Doubler\Generator\Node\ClassNode $node
+     * @param \Prophecy\Doubler\Generator\Node\MethodNode $method1
+     * @param \Prophecy\Doubler\Generator\Node\MethodNode $method2
+     * @param \Prophecy\Doubler\Generator\Node\MethodNode $method3
      */
-    function it_will_remove_echo_and_eval_methods($node)
+    function it_will_remove_echo_and_eval_methods($node, $method1, $method2, $method3)
     {
         $node->removeMethod('eval')->shouldBeCalled();
         $node->removeMethod('echo')->shouldBeCalled();
 
-        $node->getMethods()->willReturn(
-            array(
-                'echo' => new MethodNode('echo'),
-                'eval' => new MethodNode('eval'),
-                'notKeyword' => new MethodNode('notKeyword')
-            )
-        );
+        $method1->getName()->willReturn('echo');
+        $method2->getName()->willReturn('eval');
+        $method3->getName()->willReturn('notKeyword');
+
+        $node->getMethods()->willReturn(array(
+            'echo' => $method1,
+            'eval' => $method2,
+            'notKeyword' => $method3,
+        ));
 
         $this->apply($node);
     }
