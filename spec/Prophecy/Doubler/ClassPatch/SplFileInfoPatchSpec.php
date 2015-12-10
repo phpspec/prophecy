bda7bc8a072f37b7fbcfee73b8ef3bd6089be4b4
Kamil Kokot <kamil@kokot.me>
2015-12-10 15:47:46 +0100

    Patched \SplFileObject constructor while using \SplFileInfo::openDir()

--- spec/Prophecy/Doubler/ClassPatch/SplFileInfoPatchSpec.php
+++ spec/Prophecy/Doubler/ClassPatch/SplFileInfoPatchSpec.php
@@ -88,4 +88,20 @@ class SplFileInfoPatchSpec extends ObjectBehavior
         $this->apply($node);
     }
 
+    /**
+     * @param \Prophecy\Doubler\Generator\Node\ClassNode  $node
+     * @param \Prophecy\Doubler\Generator\Node\MethodNode $method
+     */
+    function it_should_supply_a_file_for_a_spl_file_object($node, $method)
+    {
+        $node->hasMethod('__construct')->willReturn(true);
+        $node->getMethod('__construct')->willReturn($method);
+        $node->getParentClass()->willReturn('SplFileObject');
+
+        $method->setCode(Argument::that(function($value) {
+            return strpos($value, '.php') !== false;
+        }))->shouldBeCalled();
+
+        $this->apply($node);
+    }
 }
