6d73d78b8f85f91babcfc579e997d3ec321e1f3e
Théo FIDRY <theo.fidry@gmail.com>
2016-03-17 10:06:06 +0000

    Add tag retriever

---
+++ src/Prophecy/PhpDocumentor/MethodTagRetrieverInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Prophecy.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *     Marcello Duarte <marcello.duarte@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Prophecy\PhpDocumentor;
+
+use phpDocumentor\Reflection\DocBlock\Tag\MethodTag as LegacyMethodTag;
+use phpDocumentor\Reflection\DocBlock\Tags\Method;
+
+/**
+ * @author Théo FIDRY <theo.fidry@gmail.com>
+ */
+interface MethodTagRetrieverInterface
+{
+    /**
+     * @param \ReflectionClass $reflectionClass
+     *
+     * @return LegacyMethodTag[]|Method[]
+     */
+    public function getTagList(\ReflectionClass $reflectionClass);
+}
