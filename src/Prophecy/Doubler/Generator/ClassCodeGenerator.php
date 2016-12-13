ab39409d17d5b9c214d7eb30d065d45373b3024a
Thomas Vargiu <tvargiu@gmail.com>
2016-12-13 13:14:24 +0100

    Added support for PHP 7.1 nullable types and iterable type

--- src/Prophecy/Doubler/Generator/ClassCodeGenerator.php
+++ src/Prophecy/Doubler/Generator/ClassCodeGenerator.php
@@ -76,11 +76,24 @@ class ClassCodeGenerator
         return array_map(function (Node\ArgumentNode $argument) {
             $php = '';
 
+            if (version_compare(PHP_VERSION, '7.1', '>=')) {
+                $php .= $argument->isNullable() ? '?' : '';
+            }
+
             if ($hint = $argument->getTypeHint()) {
                 switch ($hint) {
                     case 'array':
                     case 'callable':
                         $php .= $hint;
+                        break;
+
+                    case 'iterable':
+                        if (version_compare(PHP_VERSION, '7.1', '>=')) {
+                            $php .= $hint;
+                            break;
+                        }
+
+                        $php .= '\\'.$hint;
                         break;
 
                     case 'string':
