d7407e0d62a813bece7da25953b28cd55e8ffb4f
Grégoire Paris <postmaster@greg0ire.fr>
2018-02-11 18:30:59 +0100

    Add missing expectations
    I do not know how this was supposed to work before.

--- spec/Prophecy/Prediction/NoCallsPredictionSpec.php
+++ spec/Prophecy/Prediction/NoCallsPredictionSpec.php
@@ -26,6 +26,7 @@ class NoCallsPredictionSpec extends ObjectBehavior
         Call $call,
         ArgumentsWildcard $arguments
     ) {
+        $object->reveal()->willReturn(new \stdClass);
         $method->getObjectProphecy()->willReturn($object);
         $method->getMethodName()->willReturn('getName');
         $method->getArgumentsWildcard()->willReturn($arguments);
