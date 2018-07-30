83c02a6ea2e3a43be8bc1d97a171400cb35a9011
Ciaran McNulty <mail@ciaranmcnulty.com>
2018-07-30 17:18:47 +0100

    Remove a level of calls in ThrowablePatch

--- src/Prophecy/Doubler/ClassPatch/ThrowablePatch.php
+++ src/Prophecy/Doubler/ClassPatch/ThrowablePatch.php
@@ -11,7 +11,6 @@ class ThrowablePatch implements ClassPatchInterface
      * Checks if patch supports specific class node.
      *
      * @param ClassNode $node
-     *
      * @return bool
      */
     public function supports(ClassNode $node)
@@ -25,28 +24,8 @@ class ThrowablePatch implements ClassPatchInterface
      */
     private function implementsAThrowableInterface(ClassNode $node)
     {
-        return $this->containsThrowable($node->getInterfaces());
-    }
-
-    /**
-     * @param ClassNode $node
-     * @return bool
-     */
-    private function doesNotExtendAThrowableClass(ClassNode $node)
-    {
-        return !$this->isThrowable($node->getParentClass());
-    }
-
-    /**
-     * Does a list of types contain any throwables
-     *
-     * @param array $types
-     * @return boolean
-     */
-    public function containsThrowable($types)
-    {
-        foreach ($types as $type) {
-            if ($this->isThrowable($type)) {
+        foreach ($node->getInterfaces() as $type) {
+            if (is_a($type, 'Throwable', true)) {
                 return true;
             }
         }
@@ -55,12 +34,12 @@ class ThrowablePatch implements ClassPatchInterface
     }
 
     /**
-     * @param string $type
+     * @param ClassNode $node
      * @return bool
      */
-    private function isThrowable($type)
+    private function doesNotExtendAThrowableClass(ClassNode $node)
     {
-        return is_a($type, 'Throwable', true);
+        return !is_a($node->getParentClass(), 'Throwable', true);
     }
 
     /**
@@ -76,9 +55,6 @@ class ThrowablePatch implements ClassPatchInterface
         $this->setParentClassToException($node);
     }
 
-    /**
-     * @param ClassNode $node
-     */
     private function checkItCanBeDoubled(ClassNode $node)
     {
         $className = $node->getParentClass();
@@ -93,9 +69,6 @@ class ThrowablePatch implements ClassPatchInterface
         }
     }
 
-    /**
-     * @param ClassNode $node
-     */
     private function setParentClassToException(ClassNode $node)
     {
         $node->setParentClass('Exception');
